f10fc7b8b6c92e7e4a569fe41203fd5bf5e105e8
Karsten Maurer <karstenm@iastate.edu>
2017-07-10 10:46:38 -0400

    Add functions for making ensemble building and evaluation easier

--- ExampleRunFile.R
+++ ExampleRunFile.R
@@ -37,31 +37,37 @@ train <- iris[train_index, ]
 test <- iris[-train_index, ]
 
 
-##
-## Build models to stack
-##
-my_models <- c("weka.classifiers.bayes.NaiveBayes", "weka.classifiers.trees.RandomForest")
-
-naiveBayes <- RWeka::make_Weka_classifier("weka.classifiers.bayes.NaiveBayes")
-model1 <- naiveBayes(true_class ~., train)
-
-randomForest <- RWeka::make_Weka_classifier("weka.classifiers.trees.RandomForest")
-model2 <- randomForest(true_class ~., train)
-
-bagging <- RWeka::make_Weka_classifier("weka.classifiers.meta.Bagging")
-model3 <- bagging(true_class ~., train)
-
-svm <- RWeka::make_Weka_classifier("weka.classifiers.functions.SMO")
-model4 <- svm(true_class ~., train)
-  
-modelList <- list(model1, model2, model3, model4)
+# ##
+# ## Build models to stack
+# ##
+# naiveBayes <- RWeka::make_Weka_classifier("weka.classifiers.bayes.NaiveBayes")
+# model1 <- naiveBayes(true_class ~., train)
+# 
+# randomForest <- RWeka::make_Weka_classifier("weka.classifiers.trees.RandomForest")
+# model2 <- randomForest(true_class ~., train)
+# 
+# bagging <- RWeka::make_Weka_classifier("weka.classifiers.meta.Bagging")
+# model3 <- bagging(true_class ~., train)
+# 
+# svm <- RWeka::make_Weka_classifier("weka.classifiers.functions.SMO")
+# model4 <- svm(true_class ~., train)
+#   
+# modelList <- list(model1, model2, model3, model4)
 
 
 model_types <- c("weka.classifiers.bayes.NaiveBayes","weka.classifiers.trees.RandomForest",
                  "weka.classifiers.meta.Bagging","weka.classifiers.functions.SMO")
-
-make_model_list
-
+# Function for building model list given Weka names
+make_model_list <- function(model_types, data, ...){
+  model_list <- list(NULL)
+  for(m in 1:length(model_types)){
+    mod_m_fct <- RWeka::make_Weka_classifier(model_types[m])
+    model_list[[m]] <- mod_m_fct(true_class ~., train)
+  }
+  names(model_list) <- model_types
+  return(model_list)
+}
+modelList <- make_model_list(model_types, test)
 modelList[[1]]
 
 predict(modelList[[4]], test)
@@ -85,17 +91,31 @@ weightedEnsemble <- buildWeightedEnsemble(train, modelList, weightType, comb_rul
 head(test[,-5])
 predictEnsemble(weightedEnsemble, test[,-5])
 
-s1 <- paste("Ensemble accuracy: ", RA(table(test$true_class, predictEnsemble(weightedEnsemble, test[,-5]))), sep = "")
-s2 <- paste("Model 1 accuracy: ", RA(table(test$true_class, predict(model1, test[,-6]))), sep = "")
-s3 <- paste("Model 2 accuracy: ", RA(table(test$true_class, predict(model2, test[,-6]))), sep = "")
-s4 <- paste("Model 3 accuracy: ", RA(table(test$true_class, predict(model3, test[,-6]))), sep = "")
-s5 <- paste("Model 4 accuracy: ", RA(table(test$true_class, predict(model4, test[,-6]))), sep = "")
 
-s1
-s2
-s3
-s4
-s5
+# Function to evaluated the ensemble test accuracy against member accuracies
+eval_ensemble <- function(ensemble, test_data){
+  acc_df <- data.frame(model = c("Ensemble", names(ensemble$model_storage_list)),
+                       accuracy = NA)
+  acc_df$accuracy[1] <- RA(table(test_data$true_class, predictEnsemble(ensemble, test_data[,-which(names(test_data)=="true_class")])))
+  for(m in 1:length(ensemble$model_storage_list)){
+    acc_df$accuracy[m+1] <- RA(table(test_data$true_class, predict(ensemble$model_storage_list[[m]], test_data[,-which(names(test_data)=="true_class")])))
+  }
+  return(acc_df)
+}
+eval_ensemble(weightedEnsemble, test)
+
+
+# s1 <- paste("Ensemble accuracy: ", ), sep = "")
+# s2 <- paste("Model 1 accuracy: ", RA(table(test$true_class, predict(model1, test[,-6]))), sep = "")
+# s3 <- paste("Model 2 accuracy: ", RA(table(test$true_class, predict(model2, test[,-6]))), sep = "")
+# s4 <- paste("Model 3 accuracy: ", RA(table(test$true_class, predict(model3, test[,-6]))), sep = "")
+# s5 <- paste("Model 4 accuracy: ", RA(table(test$true_class, predict(model4, test[,-6]))), sep = "")
+# 
+# s1
+# s2
+# s3
+# s4
+# s5
 
 
 #--------------------------------------------------------------------------
